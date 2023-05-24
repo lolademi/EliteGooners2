77fbe47a7cf67d812cd0d25303c78787132737f7
Lolade <39669395+lolademi@users.noreply.github.com>
2023-05-24 08:47:45 +0100

    5-May

--- elitegooners/elitegooners/settings.py
+++ elitegooners/elitegooners/settings.py
@@ -71,13 +71,23 @@ WSGI_APPLICATION = "elitegooners.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'elitegooners',
+        'USER': 'postgres',
+        'PASSWORD': 'lolademi',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 
 # Password validation
